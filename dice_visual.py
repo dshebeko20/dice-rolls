618f408e48fcdc9d72a9433ca88b44a6009ac204
dshebeko20 <dshebeko20@gmail.com>
2025-11-03 13:12:15 +0400

    Two dice roll.

---
+++ dice_visual.py
@@ -0,0 +1,29 @@
+from die import Die
+
+import plotly.express as px
+
+# Создание двух кубиков D6
+
+die_1 = Die()
+die_2 = Die()
+
+# Моделирование серии бросков с соханением результтатов в списке.
+results = []
+
+for roll_num in range(1000):
+    result = die_1.roll() + die_2.roll()
+    results.append(result)
+
+# Анализ результатов.
+frequencies = []
+max_result = die_1.num_sides + die_2.num_sides
+poss_results = range(2, max_result + 1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Визуализация результатов.
+title = "Results of Rolling Two D6 1,000 Times"
+labels =  {'x': 'Result', 'y': 'Frecuency of Result'}
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+fig.show()
